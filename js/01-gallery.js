0bca6e99805f31f20dbbda80528e231718c396a8
daryna-sob <dashasobka88@gmail.com>
2022-10-02 17:42:47 +0300

    finish task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -20,31 +20,11 @@ galleryRef.insertAdjacentHTML("afterbegin", renderList);
 galleryRef.addEventListener("click", selectImageBigSize);
 
 function selectImageBigSize(e) {
+  e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return;
   }
   const imgLink = e.target.dataset.source;
-  console.log(imgLink);
-  const instance = basicLightbox.create(`<img src="${imgLink}">`, {
-    onShow: onModalShow,
-    onClose: onModalClose,
-  });
+  const instance = basicLightbox.create(`<img src="${imgLink}">`, {});
   instance.show();
-}
-
-function onModalShow() {
-  window.addEventListener("keydown", onEscKeyPress);
-}
-
-function onModalClose() {
-  window.removeEventListener("keydown", onEscKeyPress);
-}
-
-function onEscKeyPress(event) {
-  const ESC_KEY_CODE = "Escape";
-  const isEscKey = event.code === ESC_KEY_CODE;
-
-  if (isEscKey) {
-    instance.close();
-  }
 }
