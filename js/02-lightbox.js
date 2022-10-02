898192547f38f7aaacbd615d98e20498c7bb2264
daryna-sob <dashasobka88@gmail.com>
2022-10-02 19:49:00 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryRef = document.querySelector(".gallery");
+const renderList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", renderList);
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
